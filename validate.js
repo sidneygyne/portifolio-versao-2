66b76c77bff998bbd5dd2f074b7d0dfb551bb5d5
Sidney Rodrigues <sidneyr_gyn@hotmail.com>
2024-04-19 13:31:26 -0300

    primeiro commit

---
+++ validate.js
@@ -0,0 +1,31 @@
+
+
+// function Enviar() {
+
+// var nome = document.getElementById("nome");
+// var email = document.getElementById("mail");
+// var assunto = document.getElementById("assunto");
+
+// email.addEventListener("input", function (event) {
+//   if (email.validity.typeMismatch) {
+//     email.setCustomValidity("spero um e-mail, querida!");
+//   } else {
+//     email.setCustomValidity("");
+//   }
+// });
+
+// }
+
+
+
+// function Enviar() {
+
+//     var nome = document.getElementById("nome");
+
+//     if (nome.value != "") (nome.value != ""){
+//         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso!');
+//     }
+
+
+
+}
